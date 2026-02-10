1b186cfac60eae7373f7d833dbd8bed40ba57027
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-10 09:46:31 +0100

    replace app placeholder with header and router outlet, add tailwind import, and configure routes for register, login, and jobs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/components/register/register').then(m => m.Register)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login)
+  },{
+    path: 'jobs',
+    loadComponent: () => import('./features/jobs/components/job-container/job-container').then(m => m.JobContainer)
+  }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './core/layouts/header/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
