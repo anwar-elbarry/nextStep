71d43e923cc2943a362cc4367305227533718de9
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-11 14:49:50 +0100

    update job card to use routerLink for details button and expand description preview to 3 lines

--- src/app/features/jobs/components/job-card/job-card.ts
+++ src/app/features/jobs/components/job-card/job-card.ts
@@ -1,9 +1,10 @@
 import {Component, input} from '@angular/core';
 import {JobModel} from '../../models/job.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-job-card',
-  imports: [],
+  imports: [RouterLink],
   standalone:true,
   templateUrl: './job-card.html',
   styleUrl: './job-card.css',
