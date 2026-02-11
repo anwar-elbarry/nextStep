51b9cfa5c94898d84f5df6f9c3fa98e17de6eaad
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-11 14:49:23 +0100

    update jobs route to use loadChildren with jobRouter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,6 @@ export const routes: Routes = [
     loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login)
   },{
     path: 'jobs',
-    loadComponent: () => import('./features/jobs/components/job-container/job-container').then(m => m.JobContainer)
-  }
+    loadChildren: () => import('./features/jobs/job.routers').then(m => m.jobRouter)
+  },
 ];
