b34e23dbc7c43bf8cfaacfebfe542f2fa320dae1
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-11 14:49:55 +0100

    refactor job container to use router outlet and remove job list rendering logic

--- src/app/features/jobs/components/job-container/job-container.ts
+++ src/app/features/jobs/components/job-container/job-container.ts
@@ -1,31 +1,15 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
-import {JobCard} from '../job-card/job-card';
-import {JobService} from '../../job.service';
-import {JobModel} from '../../models/job.model';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-job-container',
   imports: [
-    JobCard
-  ],
+    RouterOutlet
+],
   standalone: true,
   templateUrl: './job-container.html',
   styleUrl: './job-container.css',
 })
-export class JobContainer implements OnInit {
-    private  jobService =  inject(JobService);
-    jobsList = signal<JobModel[]>([]);
+export class JobContainer {
 
-    ngOnInit() {
-      this.getJobs();
-    }
-
-    getJobs(){
-      this.jobService.getArbetNowJobs().subscribe({
-        next: (jobs) => {
-          this.jobsList.set(jobs);
-          console.log(this.jobsList());
-        }
-      })
-    }
 }
