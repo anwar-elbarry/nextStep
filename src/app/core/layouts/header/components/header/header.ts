fe367059fe861a71c12bf033780725597e0cc473
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-12 16:01:21 +0100

    update Font Awesome version and replace href with routerLink in header navigation

--- src/app/core/layouts/header/components/header/header.ts
+++ src/app/core/layouts/header/components/header/header.ts
@@ -1,13 +1,12 @@
 import {Component, inject} from '@angular/core';
 import {AuthService} from '../../../../../features/auth/auth.service';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone:true,
   imports: [
     RouterLink,
-    RouterLinkActive
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
