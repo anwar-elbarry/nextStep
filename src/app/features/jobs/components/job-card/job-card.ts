b20eb53e7fe18c23633fcd3c53bbd2901fef385a
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-12 16:01:35 +0100

    refactor JobCard component to enhance job details retrieval and update template for location display

--- src/app/features/jobs/components/job-card/job-card.ts
+++ src/app/features/jobs/components/job-card/job-card.ts
@@ -1,14 +1,26 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {JobModel} from '../../models/job.model';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-job-card',
-  imports: [RouterLink],
   standalone:true,
   templateUrl: './job-card.html',
   styleUrl: './job-card.css',
 })
 export class JobCard {
+
+      private router = inject(Router);
       job = input<JobModel>();
+      country = input<string>();
+      page = input<number>();
+
+      getJobDetails(id:string){
+          this.router.navigate(['/jobs',id],{
+            state: {
+              country : this.country(),
+              page : this.page()
+            }
+          });
+      }
 }
