92a1e3a4ccbf70248c53057b730785fa847647aa
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-13 11:50:47 +0100

    feat: add action types for managing favorite job offers in NgRx store

---
+++ src/app/core/store/actionTypes/FavoritesOffers.types.ts
@@ -0,0 +1,6 @@
+export enum FavoritesOffersActionTypes {
+  AddToFavorites = '[FavoritesOffers] Add To Favorites',
+  RemoveFromFavorites = '[FavoritesOffers] Remove From Favorites',
+  Loading = '[FavoritesOffers] Loading',
+  Error = '[FavoritesOffers] Error',
+}
