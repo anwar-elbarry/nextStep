8fa4e061f234bc6be4512b13e6e0022bd6ef383e
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-13 11:51:06 +0100

    feat: remove action types for managing favorite job offers in NgRx store

--- src/app/core/store/actionTypes/FavoritesOffers.types.ts
+++
@@ -1,6 +0,0 @@
-export enum FavoritesOffersActionTypes {
-  AddToFavorites = '[FavoritesOffers] Add To Favorites',
-  RemoveFromFavorites = '[FavoritesOffers] Remove From Favorites',
-  Loading = '[FavoritesOffers] Loading',
-  Error = '[FavoritesOffers] Error',
-}
