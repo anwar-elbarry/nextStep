67b4e6f237c7653af1d22c6d42d0a97f6a2a3366
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-14 22:41:39 +0100

    refactor app configuration and HTML structure for improved organization and clarity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { favoriteJobsReducer } from './core/store/reducers/favoritesOffers.reducer';
+import { FavoritesOffersEffects } from './core/store/effects/favoritesOffers.effect';
+import { authReducer } from './core/store/reducers/auth.reducer';
+import { AuthEffects } from './core/store/effects/auth.effect';
+import { alertReducer } from './core/store/reducers/alert.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes,withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    provideStore({
+      favoritesOffers: favoriteJobsReducer,
+      auth: authReducer,
+      alerts: alertReducer
+    }),
+    provideEffects(FavoritesOffersEffects, AuthEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true,
+      },
+    }),
   ]
 };
