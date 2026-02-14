78405333b1ae70bbc9f5f5c3474d48198c954659
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-14 23:22:39 +0100

    feat(favorites): implement user-based fetching of favorite jobs

--- src/app/features/favorites/favoriteJobs.service.ts
+++ src/app/features/favorites/favoriteJobs.service.ts
@@ -2,8 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
 import { FavoriteJobRespModel } from './models/favoriteJob-resp.model';
 import { FavoriteJobReqModel } from './models/favoriteJob-req.model';
+import { Store } from '@ngrx/store';
+import { selectUserId } from '../../core/store/selectors/auth.selectors';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +14,15 @@ import { FavoriteJobReqModel } from './models/favoriteJob-req.model';
 export class FavoriteJobsService {
   private http = inject(HttpClient);
   private apiUrl = `${environment.jsonServerUrl}/favoritesOffers`;
+  private store = inject(Store);
 
   getFavoritesJobs(): Observable<FavoriteJobRespModel[]> {
-    return this.http.get<FavoriteJobRespModel[]>(`${this.apiUrl}`);
+    return this.store.select(selectUserId).pipe(
+      take(1),
+      switchMap(userId =>
+        this.http.get<FavoriteJobRespModel[]>(`${this.apiUrl}?userId=${userId}`)
+      )
+    );
   }
 
   addFavoritesJobs(job: FavoriteJobReqModel): Observable<FavoriteJobRespModel> {
