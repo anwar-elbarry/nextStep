baa29148b51071705145e56ec3db38680073eafc
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-14 22:46:53 +0100

    style(job-details-resolver): format code for improved readability and consistency

--- src/app/features/jobs/resolvers/job-details-resolver.ts
+++ src/app/features/jobs/resolvers/job-details-resolver.ts
@@ -1,20 +1,20 @@
-import {ActivatedRouteSnapshot, ResolveFn} from '@angular/router';
-import {inject, Injectable} from '@angular/core';
-import {JobModel} from '../models/job.model';
+import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { JobModel } from '../models/job.model';
 import { JobService } from '../job.service';
 
 Injectable({
   providedIn: 'root'
 })
-export const jobDetailsResolver: ResolveFn<JobModel> = (route:ActivatedRouteSnapshot) => {
+export const jobDetailsResolver: ResolveFn<JobModel> = (route: ActivatedRouteSnapshot) => {
 
-  const id  = route.paramMap.get('id')!;
+  const id = route.paramMap.get('id')!;
   const state = history.state;
   const country = state?.country || 'us';
   const page = state?.page || 1;
-  
+
   const jobService = inject(JobService);
-  console.log(id,country,page);
-  return jobService.getJob(id,{country,page});
+  console.log(id, country, page);
+  return jobService.getJob(id, { country, page });
 
 };
