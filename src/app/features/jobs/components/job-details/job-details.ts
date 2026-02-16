e92e0cc531c42f8c524569aaceb85f8fb225f250
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-16 15:37:26 +0000

    feat(job-details): enhance job details layout and simplify component structure

--- src/app/features/jobs/components/job-details/job-details.ts
+++ src/app/features/jobs/components/job-details/job-details.ts
@@ -1,20 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { JobModel } from '../../models/job.model';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TimeAgoPipe } from '../../../../shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-job-details',
-  standalone:true,
-  imports: [RouterLink],
+  standalone: true,
+  imports: [TimeAgoPipe],
   templateUrl: './job-details.html',
   styleUrl: './job-details.css',
 })
-export class JobDetails implements OnInit{
-  private route  = inject(ActivatedRoute);
-
-  job :JobModel | undefined;
-
-  ngOnInit(): void {
-    this.route.data.subscribe(data => this.job = data['job']);
-  }
+export class JobDetails {
+  job = input<JobModel>();
 }
