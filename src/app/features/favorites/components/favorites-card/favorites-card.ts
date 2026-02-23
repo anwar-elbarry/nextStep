7412f05834484e69180ea5d4c47d46e5b87acd2e
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-23 21:14:51 +0000

    refactor(favorites-card): remove unused imports to clean up code

--- src/app/features/favorites/components/favorites-card/favorites-card.ts
+++ src/app/features/favorites/components/favorites-card/favorites-card.ts
@@ -1,15 +1,8 @@
-import { Component, computed, inject, input, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FavoriteJobRespModel } from '../../models/favoriteJob-resp.model';
 import { FavoritesOffersPage } from '../../../../core/store/actions/favoritesOffers.actions';
 import { FavjobDetails } from '../favjob-details/favjob-details';
-import { selectAllApplications, selectApplicationById } from '../../../../core/store/selectors/application.selectors';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { JobModel } from '../../../jobs/models/job.model';
-import { ApplicationReqModel } from '../../../applications/models/applicationReq.model';
-import { selectUserId } from '../../../../core/store/selectors/auth.selectors';
-import { ApplicationStatus } from '../../../applications/models/applicationStatus.enum';
-import { ApplicationActions } from '../../../../core/store/actions/application.actions';
 
 @Component({
   selector: 'app-favorites-card',
