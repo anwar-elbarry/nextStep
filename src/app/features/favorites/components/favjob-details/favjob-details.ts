495e4e1e9aafafaeaf6833cf1b5888493be9b4d9
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-23 21:02:02 +0000

    feat(favjob-details): add tracking functionality for job applications

--- src/app/features/favorites/components/favjob-details/favjob-details.ts
+++ src/app/features/favorites/components/favjob-details/favjob-details.ts
@@ -1,8 +1,15 @@
-import { Component, inject, input, OnInit, output, signal } from '@angular/core';
+import { Component, computed, inject, input, OnInit, output, signal } from '@angular/core';
 import { FavoriteJobRespModel } from '../../models/favoriteJob-resp.model';
 import { JobService } from '../../../jobs/job.service';
 import { JobModel } from '../../../jobs/models/job.model';
 import { TimeAgoPipe } from '../../../../shared/pipes/time-ago.pipe';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Store } from '@ngrx/store';
+import { selectAllApplications } from '../../../../core/store/selectors/application.selectors';
+import { selectUserId } from '../../../../core/store/selectors/auth.selectors';
+import { ApplicationReqModel } from '../../../applications/models/applicationReq.model';
+import { ApplicationStatus } from '../../../applications/models/applicationStatus.enum';
+import { ApplicationActions } from '../../../../core/store/actions/application.actions';
 
 @Component({
   selector: 'app-favjob-details',
@@ -15,12 +22,51 @@ export class FavjobDetails implements OnInit {
   job = input.required<FavoriteJobRespModel>();
   close = output<void>();
 
+  private store = inject(Store);
+
   private jobService = inject(JobService);
   jobDetails = signal<JobModel | null>(null);
+
+  applications = toSignal(this.store.select(selectAllApplications), {initialValue:[]});
+  userId = toSignal(this.store.select(selectUserId),{initialValue:''});
 
   ngOnInit(): void {
     this.getJob();
   }
+
+  
+    transformToApplication(job: JobModel): ApplicationReqModel {
+      return {
+        userId: this.userId(),
+        offerId: job.id,
+        apiSource: 'adzuna',
+        title: job.jobTitle,
+        company: job.companyName,
+        location: job.location,
+        url: job.fullOfferLink || '',
+        status: ApplicationStatus.PENDING,
+        notes: ''
+      };
+  
+    }
+
+
+    trackApplication(){
+      if (this.isTracked()) {
+        console.warn('Job already tracked');
+        return;
+      }
+      const application = this.transformToApplication(this.jobDetails()!);
+      this.store.dispatch(ApplicationActions.addApplication({ application }));
+    }
+
+
+    isTracked = computed(() => {
+        
+        const jobId = this.job()?.offerId;
+        if (!jobId) return false;
+        return this.applications().some(app => app.offerId === jobId);
+  });
 
   getJob() {
     const fav = this.job();
@@ -28,6 +74,8 @@ export class FavjobDetails implements OnInit {
       country: fav.country || 'us',
       page: 1
     };
+    console.log(pathVariables);
+    
     this.jobService.getJob(fav.offerId, pathVariables, { what: fav.title }).subscribe(job => {
       this.jobDetails.set(job);
     });
