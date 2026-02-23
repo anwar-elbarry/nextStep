47a804cba808b6c89785b2b8d1786cfac4d7a58c
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-23 21:02:08 +0000

    feat(favorites-card): enhance component with additional imports and clean up comments

--- src/app/features/favorites/components/favorites-card/favorites-card.ts
+++ src/app/features/favorites/components/favorites-card/favorites-card.ts
@@ -1,8 +1,15 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FavoriteJobRespModel } from '../../models/favoriteJob-resp.model';
 import { FavoritesOffersPage } from '../../../../core/store/actions/favoritesOffers.actions';
 import { FavjobDetails } from '../favjob-details/favjob-details';
+import { selectAllApplications, selectApplicationById } from '../../../../core/store/selectors/application.selectors';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { JobModel } from '../../../jobs/models/job.model';
+import { ApplicationReqModel } from '../../../applications/models/applicationReq.model';
+import { selectUserId } from '../../../../core/store/selectors/auth.selectors';
+import { ApplicationStatus } from '../../../applications/models/applicationStatus.enum';
+import { ApplicationActions } from '../../../../core/store/actions/application.actions';
 
 @Component({
   selector: 'app-favorites-card',
@@ -13,25 +20,22 @@ import { FavjobDetails } from '../favjob-details/favjob-details';
 export class FavoritesCard {
   private store = inject(Store);
 
-  // Input property for favorite job data
   favorite = input<FavoriteJobRespModel>();
 
-  // Popup state
   showDetails = signal(false);
 
-  // Open job details popup
+
   openDetails() {
     this.showDetails.set(true);
   }
 
-  // Close job details popup
   closeDetails() {
     this.showDetails.set(false);
   }
 
   // Remove job from favorites
   removeFromFavorites(offerId: string, event: Event) {
-    event.stopPropagation(); // Prevent card click navigation
+    event.stopPropagation();
     this.store.dispatch(FavoritesOffersPage.removeFromFavorites({ offerId }));
   }
 }
